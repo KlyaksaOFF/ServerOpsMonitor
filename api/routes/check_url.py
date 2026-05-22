285ccf4e905d6115e2a378b8a65fd0e5c5ef9400
Alex <klyaksaoff@gmail.com>
2026-05-22 20:19:56 +0300

    updated check_url

--- api/routes/check_url.py
+++ api/routes/check_url.py
@@ -1,14 +1,27 @@
-from fastapi import APIRouter, Request
+import requests
+from typing import Annotated
+from fastapi import APIRouter, Form, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-
+templates = Jinja2Templates(directory="api/templates")
 
 @router.get('/checkurl/', response_class=HTMLResponse)
 async def checkurl_main(request: Request):
-    pass
+    response = templates.TemplateResponse(
+        name='check_url.html', request=request)
+    return response
 
 
 @router.post('/checkurl/', response_class=HTMLResponse)
-async def checkurl_result(request: Request):
-    pass
+async def checkurl_result(request: Request, url: Annotated[str, Form()]):
+    r = requests.get(url)
+
+    response = templates.TemplateResponse(
+        name='check_url.html', request=request, context={
+            'status': r.status_code,
+            'encoding': r.encoding,
+            'headers': r.headers,
+        })
+    return response
