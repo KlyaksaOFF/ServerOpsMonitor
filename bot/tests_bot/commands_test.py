e79f970c71ac89be4c579b7c44abfe4a05ee09a9
Alex <klyaksaoff@gmail.com>
2026-06-06 20:27:36 +0300

    added tests in commands_test

--- bot/tests_bot/commands_test.py
+++ bot/tests_bot/commands_test.py
@@ -48,7 +48,7 @@ async def test_start(telegram_client):
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_list_servers(telegram_client):
+async def test_list_servers (telegram_client):
     bot_login = (getenv("BOT_LOGIN"))
 
     async with telegram_client.conversation(bot_login) as conv:
@@ -57,3 +57,27 @@ async def test_list_servers(telegram_client):
         response = await conv.get_response()
         assert response is not None
         assert response.text == "You don't have servers"
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_add_server (telegram_client):
+    bot_login = (getenv("BOT_LOGIN"))
+
+    async with telegram_client.conversation(bot_login) as conv:
+        await conv.send_message('Add server')
+
+        response = await conv.get_response()
+        assert response is not None
+        assert response.text == "Enter the server IP"
+
+        await conv.send_message('192.168.1.1')
+
+        response = await conv.get_response()
+        assert response is not None
+        assert response.text == "Send password for ip"
+
+        await conv.send_message('123456')
+
+        response = await conv.get_response()
+        assert response is not None
+        assert response.text == "New server created in your list"
